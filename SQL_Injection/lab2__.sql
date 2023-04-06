a49eaa84e3db21262b62363dcd2a6097f91ca8ed
unknown <akayali532@gmail.com>
2023-04-06 17:07:36 +0200

    addiing mroe SQL labs

--- SQL_Injection/lab2__.sql
+++
@@ -1,8 +0,0 @@
---Name: SQL injection UNION attack, determining the number of columns returned by the query
---URL:https://portswigger.net/web-security/sql-injection/lab-login-bypass
---Level:Easy Time: 5 min
----------------------------------------------------------------------------------
-SELECT * FROM users WHERE name='administrator' and password='?'
-
-
-SELECT * FROM users WHERE name='administrator'-- and password='?'   Solved
